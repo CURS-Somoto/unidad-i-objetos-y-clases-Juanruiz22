0f519c70d3dbca46b88758c24fee6451a94e7f97
Juanruiz22 <147443872+Juanruiz22@users.noreply.github.com>
2023-10-21 04:26:43 +0200

    Update pelicula.cs
    clase pelicula

--- pelicula.cs
+++ pelicula.cs
@@ -1,17 +1,49 @@
 using System;
 
-public class Película
+namespace ClasePelicula
 {
-    
-    public string Titulo { get; set; }
-    public int AnyoLanzamiento { get; set; }
-   
-}
-
-class Program
-{
-    static void Main(string[] args)
+    public class Pelicula
     {
-        Console.WriteLine("¡Hola, mundo!");
+        public string Titulo { get; set; }
+        public int AñoDeLanzamiento { get; set; }
+        public int Calificacion { get; set; }
+
+        public Pelicula()
+        {
+            Calificacion = 0;
+        }
+
+        public Pelicula(string titulo, int anioDeLanzamiento)
+        {
+            Titulo = titulo;
+            AñoDeLanzamiento = anioDeLanzamiento;
+            Calificacion = 0;
+        }
+
+        public void Calificar(int calificacion)
+        {
+            if (calificacion >= 1 && calificacion <= 5)
+            {
+                Calificacion = calificacion;
+            }
+            else
+            {
+                Console.WriteLine("La calificación debe ser entre 1 y 5");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pelicula pelicula1 = new Pelicula("El Señor de los Anillos", 2001);
+            pelicula1.Calificar(4);l
+            Console.WriteLine("La calificación de la película es: {0}", pelicula1.Calificacion);
+            Pelicula pelicula2 = new Pelicula("Cien años de soledad", 1967);
+            pelicula2.Calificar(5);
+            Console.WriteLine("La calificación de la película es: {0}", pelicula2.Calificacion);
+            Console.ReadKey();
+        }
     }
 }
