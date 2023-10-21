7f29ab13e5cfb8aad3ad7a7ba86ef55d8d8519b8
Juanruiz22 <147443872+Juanruiz22@users.noreply.github.com>
2023-10-21 04:24:25 +0200

    Update libro.cs
    trabajo de poo, clase libro

--- libro.cs
+++ libro.cs
@@ -1,19 +1,35 @@
 using System;
 
-public class Libro
+namespace ClaseLibro
 {
-    // Propiedades
-    public string Título { get; set; }
-    public string Autor { get; set; }
-
-  
-   
-}
-
-class Program
-{
-    static void Main(string[] args)
+    public class Libro
     {
-        Console.WriteLine("¡Hola, mundo!");
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine("Título: {0}", Titulo);
+            Console.WriteLine("Autor: {0}", Autor);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Libro libro1 = new Libro();
+            libro1.Titulo = "El Señor de los Anillos";
+            libro1.Autor = "J.R.R. Tolkien";
+
+            Libro libro2 = new Libro();
+            libro2.Titulo = "Cien años de soledad";
+            libro2.Autor = "Gabriel García Márquez";
+
+            libro1.MostrarInformacion();
+            libro2.MostrarInformacion();
+            Console.ReadKey();
+        }
     }
 }
