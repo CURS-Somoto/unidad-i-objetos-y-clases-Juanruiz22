1fe5370ed38157a0fe6b8376cc136ca13f9bd551
Juanruiz22 <147443872+Juanruiz22@users.noreply.github.com>
2023-10-21 04:23:16 +0200

    Update estudiante.cs
    trabajo clase estudiante

--- estudiante.cs
+++ estudiante.cs
@@ -1,20 +1,40 @@
 using System;
 
-public class Estudiante
+namespace ClaseEstudiante
 {
-   
-    public string Nombre { get; set; }
-    public int Edad { get; set; }
-    public string Curso { get; set; }
-
-    
-   
-}
-
-class Program
-{
-    static void Main(string[] args)
+    public class Estudiante
     {
-        Console.WriteLine("¡Hola, mundo!");
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Curso { get; set; }
+
+        public Estudiante(string nombre, int edad, string curso)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Curso = curso;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Estudiante estudiante1 = new Estudiante("Juan ruiz", 20, "Ingeniería de Software");
+            Estudiante estudiante2 = new Estudiante("francelys vanegas", 18, "ing diseño web");
+
+            Console.WriteLine("Información del estudiante 1:");
+            Console.WriteLine("Nombre: {0}", estudiante1.Nombre);
+            Console.WriteLine("Edad: {0}", estudiante1.Edad);
+            Console.WriteLine("Curso: {0}", estudiante1.Curso);
+
+            Console.WriteLine("Información del estudiante 2:");
+            Console.WriteLine("Nombre: {0}", estudiante2.Nombre);
+            Console.WriteLine("Edad: {0}", estudiante2.Edad);
+            Console.WriteLine("Curso: {0}", estudiante2.Curso);
+
+            Console.ReadKey();
+        }
     }
 }
