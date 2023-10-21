225c51b8f83c9febcaed883bb45b857e4205c428
Juanruiz22 <147443872+Juanruiz22@users.noreply.github.com>
2023-10-21 04:21:54 +0200

    Update empleado.cs
    trabajo clase empleado

--- empleado.cs
+++ empleado.cs
@@ -1,22 +1,34 @@
 using System;
 
-public class Empleado
+namespace ClaseEmpleado
 {
-   
-    public string Nombre { get; set; }
-    public double Salario { get; set; }
-
-    
-    public double CalcularSalarioAnual()
+    public class Empleado
     {
-        
+        public string Nombre { get; set; }
+        public double Salario { get; set; }
+
+        public double CalcularSalarioAnual()
+        {
+            return Salario * 12;
+        }
     }
-}
 
-class Program
-{
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("¡Hola, mundo!");
+        static void Main(string[] args)
+        {
+            Empleado empleado1 = new Empleado();
+            empleado1.Nombre = "Juan ruiz";
+            empleado1.Salario = 1000;
+
+            Empleado empleado2 = new Empleado();
+            empleado2.Nombre = "María García";
+            empleado2.Salario = 2000;
+
+            Console.WriteLine("Salario anual de Juan Pérez: {0}", empleado1.CalcularSalarioAnual());
+            Console.WriteLine("Salario anual de María García: {0}", empleado2.CalcularSalarioAnual());
+
+            Console.ReadKey();
+        }
     }
 }
